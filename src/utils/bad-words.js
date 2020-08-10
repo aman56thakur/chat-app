7e6b668d62b65b29b39a9e4009552739fe1d1854
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-10 22:39:49 +0530

    init commit

---
+++ src/utils/bad-words.js
@@ -0,0 +1,31 @@
+const badWords = [
+    'lund',
+    'choot',
+    'gand',
+    'gaand',
+    'lauda',
+    'madharchod',
+    'behenchod',
+    'bhosdi',
+    'bhosdike',
+    'bhosdika',
+    'laude',
+    'lawda',
+    'loda',
+    'bakland',
+    'chinaal',
+    'chutia',
+    'chutiya',
+    'harami',
+    'tatte',
+    'jhant',
+    'jhaant',
+    'randi',
+    'rundi',
+    'betichod',
+    'chut',
+    'chodu',
+    'gandu'    
+]
+
+module.exports = badWords
