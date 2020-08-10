249fc3818f2a85fcc1a06b3601d311481813ce93
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-10 22:54:02 +0530

    formatted

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,33 +1,33 @@
 const users = []
 
-const addUser = ({
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+  if (!username || !room)
+    return {
+      error: 'Username and room are required',
+    }
+  const existingUser = users.find(
+    (user) => user.username === username && user.room === room
+  )
+  if (existingUser)
+    return {
+      error: 'Username already taken!',
+    }
+  const user = {
     id,
     username,
-    room
-}) => {
-    username = username.trim().toLowerCase()
-    room = room.trim().toLowerCase()
-    if (!username || !room) return {
-        error: 'Username and room are required'
-    }
-    const existingUser = users.find((user) => user.username === username && user.room === room)
-    if (existingUser) return {
-        error: 'Username already taken!'
-    }
-    const user = {
-        id,
-        username,
-        room
-    }
-    users.push(user)
-    return {
-        user
-    }
+    room,
+  }
+  users.push(user)
+  return {
+    user,
+  }
 }
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id)
-    if (index !== -1) return users.splice(index, 1)[0]
+  const index = users.findIndex((user) => user.id === id)
+  if (index !== -1) return users.splice(index, 1)[0]
 }
 
 const getUser = (id) => users.find((user) => user.id === id)
@@ -35,8 +35,8 @@ const getUser = (id) => users.find((user) => user.id === id)
 const getUsersInRoom = (room) => users.filter((user) => user.room === room)
 
 module.exports = {
-    addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
 }
