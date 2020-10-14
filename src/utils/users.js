ad6cbaf4ea555bfc718b2ac26d1c45c5e1e83171
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-10-14 10:49:40 +0530

    fixed joining bug

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,12 +1,12 @@
 const users = []
 
 const addUser = ({ id, username, room }) => {
-  username = username.trim().toLowerCase()
-  room = room.trim().toLowerCase()
   if (!username || !room)
     return {
       error: 'Username and room are required',
     }
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
   const existingUser = users.find(
     (user) => user.username === username && user.room === room
   )
